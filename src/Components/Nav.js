4b6b72c50877587c32cef037f2adb7b404e9a483
mrMayankyadav <134904283+mrMayankyadav@users.noreply.github.com>
2023-06-21 10:06:02 +0530

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,14 +3,14 @@ export function Nav() {
       <div>
         <nav className="navbar navbar-expand-lg" id="navbar">
           <div className="container-fluid">
-            <a className="navbar-brand ps-5" href="/">Portfolio.</a>
+            <a className="navbar-brand ps-5" href="/React-Task-6">Portfolio.</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
-                  <a className="nav-link pe-4" href="/">Home</a>
+                  <a className="nav-link pe-4" href="/React-Task-6">Home</a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link pe-4" href="/about">About me</a>
