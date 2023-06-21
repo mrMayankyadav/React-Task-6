3d402bb34cac5851c2585393d39e5b90fec94a70
mrMayankyadav <134904283+mrMayankyadav@users.noreply.github.com>
2023-06-21 12:52:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './Style.css';
-import { Nav } from './Components/Nav';
 import { Home } from './Components/Home';
 import { About } from './Components/About';
 import { Projects } from './Components/Projects';
 import { Contact } from './Components/Contact';
-import {HashRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div >
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/React-Task-6" element={<Home/>} />
           <Route path="/About" element={<About/>} />
           <Route path="/projects" element={<Projects/>} />
           <Route path="/contact" element={<Contact/>} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
       
     </div>
   );
