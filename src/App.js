0436fdf82e903504ab5b98ac572892a3316c1cfa
mrMayankyadav <134904283+mrMayankyadav@users.noreply.github.com>
2023-06-21 12:59:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <div >
-      <BrowserRouter>
-        <Routes>
-          <Route path="/React-Task-6" element={<Home/>} />
-          <Route path="/About" element={<About/>} />
-          <Route path="/projects" element={<Projects/>} />
-          <Route path="/contact" element={<Contact/>} />
-        </Routes>
-      </BrowserRouter>
+      // <BrowserRouter>
+      //   <Routes>
+      //     <Route path="/React-Task-6" element={<Home/>} />
+      //     <Route path="/About" element={<About/>} />
+      //     <Route path="/projects" element={<Projects/>} />
+      //     <Route path="/contact" element={<Contact/>} />
+      //   </Routes>
+      // </BrowserRouter>
+      <Home/>
       
     </div>
   );
